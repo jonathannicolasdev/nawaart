3ba04f1c26a81c60b757f9834ecdd55beea7dff6
jobegood49 <jonathan.s.nicolas@gmail.com>
2020-03-30 21:19:05 -0700

    Create homepage content

--- src/App.js
+++ src/App.js
@@ -3,7 +3,40 @@ import React from "react";
 function App() {
   return (
     <div>
-      <p>Nawaart</p>
+      <div>
+        <nav>
+          <img src="/nawaart-logo.svg" alt="nawaart" />
+          <ul>
+            <li>Artists</li>
+            <li>Artworks</li>
+            <li>Stories</li>
+            <li>About</li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <h2>Billy Childish</h2>
+        <p>
+          remember all the / high and exalted things remember all the low / and
+          broken things
+        </p>
+        <p>New York, W 22nd Street</p>
+        <p>January 23 - March 21, 2020</p>
+      </div>
+      <main>
+        <h1>Stories</h1>
+        <div>
+          <img src="/assets/story.jpg" alt="Home at The Peninsula New York" />
+          <h3>Home at The Peninsula New York</h3>
+          <p>Through March 2020</p>
+          <p>
+            Lehmann Maupin presents an exhibition featuring Ashley Bickerton,
+            Heidi Bucher, Do Ho Suh, Catherine Opie, and Angel Otero that
+            showcases works addressing themes of identity, heritage, and
+            community.
+          </p>
+        </div>
+      </main>
     </div>
   );
 }
