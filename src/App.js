8cc435bdb0aabf219869d9ecdfbdf9783949cb87
jobegood49 <jonathan.s.nicolas@gmail.com>
2020-04-04 22:46:21 -0700

    Added router and artists page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
-import styled from "styled-components";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ArtistsPage from "./pages/Artists";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <HomePage></HomePage>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={HomePage} exact />
+          <Route path="/artists" component={ArtistsPage} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
