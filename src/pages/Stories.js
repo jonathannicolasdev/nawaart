e30bb62a04f4b8974cb2bb2a793db10808d57d39
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-04-13 15:53:06 -0700

    Add stories page content and style

--- src/pages/Stories.js
+++ src/pages/Stories.js
@@ -1,11 +1,40 @@
 import React from "react";
 import Header from "../components/Header";
+import styled from "styled-components";
+import storiesData from "../data/stories.json";
 
 const Stories = () => {
+  const StoriesContainer = styled.div`
+    margin: 50px;
+  `;
+
+  const StoryContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    img {
+      width: 400px;
+      height: 300px;
+    }
+    h2 {
+      width: 500px;
+    }
+  `;
+
   return (
     <div>
       <Header></Header>
-      <p>this is the stories page</p>
+
+      {storiesData.map((story, index) => {
+        return (
+          <StoriesContainer>
+            <StoryContainer key={index}>
+              <img src={story.image} alt={story.title}></img>
+              <h2>{story.title}</h2>
+            </StoryContainer>
+          </StoriesContainer>
+        );
+      })}
     </div>
   );
 };
