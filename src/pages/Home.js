a04bc148052f189394dd3ca4b18df51fe7a5c257
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-05-06 22:18:18 -0700

    Fetch actual stories from backend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Header from "../components/Header";
+import axios from "axios";
 
 const Hero = styled.div`
   height: 100vh;
@@ -43,86 +44,67 @@ const Story = styled.div`
   margin-bottom: 50px;
 `;
 
-class HomePage extends Component {
-  render() {
-    return (
-      <div>
-        <Hero>
-          <Header></Header>
-          <HeroDetails>
-            <h2>Billy Childish</h2>
-            <p>
-              remember all the / high and exalted things remember all the low /
-              and broken things
-            </p>
-            <p>New York, W 22nd Street</p>
-            <p>January 23 - March 21, 2020</p>
-          </HeroDetails>
-        </Hero>
-        <Section>
-          <h2>Stories</h2>
-          <Stories>
-            <Story>
-              <img
-                src="/assets/story.jpg"
-                alt="Home at The Peninsula New York"
-              />
-              <h3>Home at The Peninsula New York</h3>
-              <p>Through March 2020</p>
-              <p>
-                Lehmann Maupin presents an exhibition featuring Ashley
-                Bickerton, Heidi Bucher, Do Ho Suh, Catherine Opie, and Angel
-                Otero that showcases works addressing themes of identity,
-                heritage, and community.
-              </p>
-            </Story>
-            <Story>
-              <img
-                src="/assets/story.jpg"
-                alt="Home at The Peninsula New York"
-              />
-              <h3>Home at The Peninsula New York</h3>
-              <p>Through March 2020</p>
-              <p>
-                Lehmann Maupin presents an exhibition featuring Ashley
-                Bickerton, Heidi Bucher, Do Ho Suh, Catherine Opie, and Angel
-                Otero that showcases works addressing themes of identity,
-                heritage, and community.
-              </p>
-            </Story>
-            <Story>
-              <img
-                src="/assets/story.jpg"
-                alt="Home at The Peninsula New York"
-              />
-              <h3>Home at The Peninsula New York</h3>
-              <p>Through March 2020</p>
-              <p>
-                Lehmann Maupin presents an exhibition featuring Ashley
-                Bickerton, Heidi Bucher, Do Ho Suh, Catherine Opie, and Angel
-                Otero that showcases works addressing themes of identity,
-                heritage, and community.
-              </p>
-            </Story>
-            <Story>
-              <img
-                src="/assets/story.jpg"
-                alt="Home at The Peninsula New York"
-              />
-              <h3>Home at The Peninsula New York</h3>
-              <p>Through March 2020</p>
-              <p>
-                Lehmann Maupin presents an exhibition featuring Ashley
-                Bickerton, Heidi Bucher, Do Ho Suh, Catherine Opie, and Angel
-                Otero that showcases works addressing themes of identity,
-                heritage, and community.
-              </p>
-            </Story>
-          </Stories>
-        </Section>
-      </div>
-    );
-  }
-}
+const HomePage = () => {
+  const [stories, setStories] = useState([]);
+  const [error, setError] = useState();
+  const url = process.env.REACT_APP_API_URL + "/stories";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(url);
+      if (response) {
+        setStories(response.data.stories);
+        console.log(response.data.stories);
+      } else {
+        setError("Error when getting stories");
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return (
+    <div>
+      <Hero>
+        <Header></Header>
+        <HeroDetails>
+          <h2>Billy Childish</h2>
+          <p>
+            remember all the / high and exalted things remember all the low /
+            and broken things
+          </p>
+          <p>New York, W 22nd Street</p>
+          <p>January 23 - March 21, 2020</p>
+        </HeroDetails>
+      </Hero>
+      <Section>
+        <h2>Stories</h2>
+        <Stories>
+          {stories.length > 0 ? (
+            stories.map((story, index) => {
+              return (
+                <Story key={index}>
+                  <img
+                    src="/assets/story.jpg"
+                    alt="Home at The Peninsula New York"
+                  />
+                  <h3>Home at The Peninsula New York</h3>
+                  <p>Through March 2020</p>
+                  <p>
+                    Lehmann Maupin presents an exhibition featuring Ashley
+                    Bickerton, Heidi Bucher, Do Ho Suh, Catherine Opie, and
+                    Angel Otero that showcases works addressing themes of
+                    identity, heritage, and community.
+                  </p>
+                </Story>
+              );
+            })
+          ) : (
+            <div>No Stories Found</div>
+          )}
+        </Stories>
+      </Section>
+    </div>
+  );
+};
 
 export default HomePage;
