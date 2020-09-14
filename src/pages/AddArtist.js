274ce387ab127f9c84828699eb18bbd7483cae45
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-09-13 21:13:19 -0700

    Style add artist page

--- src/pages/AddArtist.js
+++ src/pages/AddArtist.js
@@ -18,11 +18,19 @@ const Form = styled.form`
 const FormGroup = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 20px;
+  margin: 20px 0px;
 `;
 
 const Label = styled.label`
   font-weight: 700;
+  h3 {
+    margin-top: 0px;
+  }
+`;
+
+const SubLabel = styled.label`
+  font-weight: 700;
+  color: #888888;
 `;
 
 const Input = styled.input`
@@ -70,7 +78,9 @@ const AddArtist = (props) => {
 
       <Form onSubmit={handleSubmit}>
         <FormGroup>
-          <Label htmlFor="name">Name</Label>
+          <Label htmlFor="name">
+            <h3>Name</h3>
+          </Label>
           <Input
             name="name"
             type="text"
@@ -86,7 +96,9 @@ const AddArtist = (props) => {
         </FormGroup>
 
         <FormGroup>
-          <Label htmlFor="photo">Photo</Label>
+          <Label htmlFor="photo">
+            <h3>Photo</h3>
+          </Label>
           <Input
             name="photo"
             type="text"
@@ -101,40 +113,44 @@ const AddArtist = (props) => {
           />
         </FormGroup>
 
-        <FormGroup>
-          <Label htmlFor="about">About</Label>
-          <TextArea
-            name="about"
-            value={artist.biography.about}
-            onChange={(event) => {
-              setArtist({
-                ...artist,
-                biography: {
-                  ...artist.biography,
-                  about: event.target.value,
-                },
-              });
-            }}
-          />
-        </FormGroup>
+        <section>
+          <h3>Biography</h3>
 
-        <FormGroup>
-          <Label htmlFor="exhibitions">Exhibitions</Label>
-          <Input
-            type="text"
-            name="exhibitions"
-            value={artist.biography.exhibitions[0]}
-            onChange={(event) => {
-              setArtist({
-                ...artist,
-                biography: {
-                  ...artist.biography,
-                  exhibitions: [event.target.value],
-                },
-              });
-            }}
-          />
-        </FormGroup>
+          <FormGroup>
+            <SubLabel htmlFor="about">About</SubLabel>
+            <TextArea
+              name="about"
+              value={artist.biography.about}
+              onChange={(event) => {
+                setArtist({
+                  ...artist,
+                  biography: {
+                    ...artist.biography,
+                    about: event.target.value,
+                  },
+                });
+              }}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <SubLabel htmlFor="exhibitions">Exhibitions</SubLabel>
+            <Input
+              type="text"
+              name="exhibitions"
+              value={artist.biography.exhibitions[0]}
+              onChange={(event) => {
+                setArtist({
+                  ...artist,
+                  biography: {
+                    ...artist.biography,
+                    exhibitions: [event.target.value],
+                  },
+                });
+              }}
+            />
+          </FormGroup>
+        </section>
 
         <FormGroup>
           <Input type="submit" value="Add Artist" />
