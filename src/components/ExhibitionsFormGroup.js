136f8882418695fd23698afafe7b248ce8f7e6cc
M Haidar Hanif <mhaidarhanif@gmail.com>
2020-09-15 11:17:02 +0700

    Style ExhibitionsFormGroup elements

--- src/components/ExhibitionsFormGroup.js
+++ src/components/ExhibitionsFormGroup.js
@@ -13,20 +13,57 @@ const SubLabel = styled.label`
   color: #888888;
 `;
 
+const ExhibitionList = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ExhibitionItem = styled.div`
+  display: flex;
+  padding: 10px 0;
+  input {
+    flex: 1;
+  }
+`;
+
 const Input = styled.input`
   font-size: 1.5rem;
   padding: 10px;
 `;
 
-const ExhibitionsFormGroup = (props) => {
+const Button = styled.button`
+  font-size: 1.5rem;
+  background-color: #ffffff;
+  border: 1px solid #333333;
+  border-radius: 2px;
+  padding: 10px 20px;
+  margin-left: 10px;
+`;
+
+const ExhibitionsFormGroup = () => {
+  const [exhibitions, setExhibitions] = useState([
+    "2020 “Perupa Tua#1 Ring Road”, Sangkring Art Space, Yogyakarta",
+    "2018 “Perupa Muda#3 Ring Road”, Sangkring Art Space, Yogyakarta",
+  ]);
+
   return (
     <FormGroup>
       <SubLabel>Exhibitions</SubLabel>
-      <Input type="text" value="" />
+      <ExhibitionList>
+        {exhibitions.map((exhibition, index) => {
+          return (
+            <ExhibitionItem key={index}>
+              <Input type="text" value={exhibition} />
+              <Button>{"-"}</Button>
+              <Button>{"+"}</Button>
+            </ExhibitionItem>
+          );
+        })}
+      </ExhibitionList>
     </FormGroup>
   );
 };
 
-ExhibitionsFormGroup.propTypes = {};
+// ExhibitionsFormGroup.propTypes = {};
 
 export default ExhibitionsFormGroup;
