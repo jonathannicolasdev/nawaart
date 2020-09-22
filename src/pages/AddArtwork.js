870dd2103207461cc139bd9808a8734824c816cd
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-09-21 22:35:43 -0700

    Start to implement that addArtworkPage

--- src/pages/AddArtwork.js
+++ src/pages/AddArtwork.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-const AddArtwork = (props) => {
-  return <div>this is the add artwork page</div>;
+import Header from "../components/Header";
+import getArtists from "../redux/actions/getArtists";
+
+const AddArtwork = ({
+  isLoading,
+  artists,
+  isAuthenticated,
+  handleGetArtists,
+}) => {
+  useEffect(() => {
+    handleGetArtists();
+  }, [handleGetArtists]);
+
+  return (
+    <div>
+      <Header></Header>
+
+      {isLoading && <p>Loading artists...</p>}
+
+      {!isLoading && artists && <div>Add Artwork Form</div>}
+    </div>
+  );
 };
 
-AddArtwork.propTypes = {};
+AddArtwork.propTypes = {
+  isLoading: PropTypes.bool,
+  artists: PropTypes.array,
+  isAuthenticated: PropTypes.bool,
+  handleGetArtists: PropTypes.func,
+};
 
-export default AddArtwork;
+const mapStateToProps = (state) => {
+  return {
+    isLoading: state.artists.isLoading,
+    artists: state.artists.data,
+    isAuthenticated: state.auth.isAuthenticated,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleGetArtists: () => dispatch(getArtists()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddArtwork);
