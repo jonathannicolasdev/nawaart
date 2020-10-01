edaf4b6de56f8a5d15bca40725e8adf9e406932f
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-09-30 23:09:32 -0700

    Style Hero for Artists page

--- src/pages/Artists.js
+++ src/pages/Artists.js
@@ -9,6 +9,28 @@ import ArtistList from "../components/ArtistList";
 
 import getArtists from "../redux/actions/getArtists";
 
+const Hero = styled.div`
+  display: flex;
+  justify-content: center;
+  color: #ffffff;
+  padding: 50px 0px;
+  background: linear-gradient(
+      0deg,
+      rgba(147, 32, 46, 0.8),
+      rgba(147, 32, 46, 0.8)
+    ),
+    url("/assets/Artists.jpg");
+  h1 {
+    font-size: 50px;
+    margin-bottom: 10px;
+    margin-top: 0px;
+  }
+  p {
+    font-size: 24px;
+    margin: 0;
+  }
+`;
+
 const LinkButton = styled(Link)`
   background-color: #333333;
   color: #ffffff;
@@ -23,6 +45,12 @@ const Artists = ({ isLoading, artists, isAuthenticated, handleGetArtists }) => {
 
   return (
     <Page>
+      <Hero>
+        <div>
+          <h1>Discover Bermese artists</h1>
+          <p>Famous maestro in the making</p>
+        </div>
+      </Hero>
       {isAuthenticated && (
         <LinkButton to="/artists/add">Add New Artist</LinkButton>
       )}
