9922b296ad149086592dc57868afe30c84ceef18
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-09-30 22:03:53 -0700

    Replace home image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const Hero = styled.div`
   height: 100vh;
-  background-image: url("/assets/hero.jpg");
+  background-image: url("/assets/Home.jpg");
   background-repeat: no-repeat;
   background-size: cover;
   display: flex;
@@ -31,14 +32,15 @@ const HomePage = () => {
       <Hero>
         <Header />
         <HeroDetails>
-          <h2>Billy Childish</h2>
-          <p>
-            remember all the / high and exalted thingsremember all the low / and
-            broken things
-          </p>
-          <p>New York, W 22nd Street</p>
-          <p>January 23 - March 21, 2020</p>
+          <h2>Contemporary</h2>
+          <h1>Artworks</h1>
+          <h2>From Myanmar</h2>
+          <Link>View Catalogue</Link>
         </HeroDetails>
+        <div>
+          <p>Panyan</p>
+          <p>the art of bricklaying and masonry</p>
+        </div>
       </Hero>
 
       <Footer />
