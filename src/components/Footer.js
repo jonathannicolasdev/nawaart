f7261560a4fe27daffdc6d6498d58df44e2a85f9
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-10-03 21:37:48 -0700

    Styled Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,66 @@ import styled from "styled-components";
 
 const FooterStyled = styled.footer`
   margin-top: 100px;
-  margin-bottom: 20px;
+  background-color: #d6b48c;
+  color: #55270d;
+`;
+
+const Sitemap = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const SitemapColumn = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 30px;
+  h3 {
+    font-size: 18px;
+    text-transform: uppercase;
+    margin: 0;
+  }
+  li {
+    font-size: 18px;
+    margin: 10px;
+  }
+`;
+
+const SocialMedia = styled.p`
   text-align: center;
+  margin: 10px;
+`;
+
+const Copyright = styled.p`
+  text-align: center;
+  padding: 50px 0;
+  margin: 0;
 `;
 
 const Footer = () => {
-  return <FooterStyled>Copyright &copy; 2020 Nawaart</FooterStyled>;
+  return (
+    <FooterStyled>
+      <Sitemap>
+        <SitemapColumn>
+          <li>
+            <h3> Nawaart</h3>
+          </li>
+          <li>About</li>
+          <li>Stories</li>
+        </SitemapColumn>
+        <SitemapColumn>
+          <li>
+            <h3>Content</h3>
+          </li>
+          <li>Artists</li>
+          <li>Artworks</li>
+        </SitemapColumn>
+      </Sitemap>
+      <SocialMedia>
+        <img src="/assets/Instagram.svg" />
+      </SocialMedia>
+      <Copyright>Copyright &copy; 2020 Nawaart</Copyright>
+    </FooterStyled>
+  );
 };
 
 export default Footer;
